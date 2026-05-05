865ff68832157c5495c330b76a6dba0414ba2c61
sujithgoud2210 <sujith200322@gmail.com>
2026-05-05 22:45:49 +0530

    maven project

---
+++ src/main/java/com/java/ecommerce/main/Main.java
@@ -0,0 +1,12 @@
+package com.java.ecommerce.main;
+
+import com.java.ecommerce.ui.MainMenuUI;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        new MainMenuUI().start();
+    }
+}
